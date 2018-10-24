b14675dc485c8966a182f3ee649297f6c134c918
Rabi Maharjan <rabee@Rabis-MacBook-Air.local>
2018-10-24 16:08:54 +0545

    hello

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Working!!!!')
+let random = (min, max) => {
+    if (min === undefined && max === undefined) {
+        return Math.random()
+    }
+    // if one parameter is passed, assign it to lonelyPar
+    let lonelyPar =
+        min === undefined ? max : max === undefined ? min : undefined
+    if (lonelyPar) {
+        ;(min = 0), (max = lonelyPar)
+    }
+    if (min > max) {
+        let temp = min
+        min = max
+        max = temp
+    }
+    return this.floor(Math.random() * (max - min + 1) + min)
+}
